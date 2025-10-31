22c622ac1c13357ddc98b6f703d5684c4df63874
Felix Bruguera <felixbruguera0@gmail.com>
2025-10-31 09:27:40 -0400

    refactor the person skeleton

--- src/components/people/PersonInfoSkeleton.jsx
+++ src/components/people/PersonInfoSkeleton.jsx
@@ -2,12 +2,18 @@ import StyledSkeleton from "../shared/StyledSkeleton"
 
 const PersonInfoSkeleton = () => {
   return (
-    <div className="px-5 flex items-start justify-start gap-2">
-      <StyledSkeleton styles="h-40 lg:h-49 w-28 lg:w-35" />
-      <div className="flex flex-col gap-5">
-        <StyledSkeleton styles="h-8 w-28 lg:w-60" />
-        <StyledSkeleton styles="h-25 lg:h-16 w-50 lg:w-100" />
-        <StyledSkeleton styles="h-10 w-0 lg:w-90" />
+    <div className="px-5 flex flex-col lg:flex-row items-center lg:items-start justify-start lg:justify-between gap-2 mb-5">
+      <StyledSkeleton styles="h-60 w-45 lg:w-50" />
+      <div className="flex flex-col lg:flex-row lg:justify-between gap-5 w-full">
+        <div className="flex flex-col gap-2 items-start">
+          <StyledSkeleton styles="h-12 w-35 lg:w-60" />
+          <StyledSkeleton styles="h-25 w-75 lg:w-100" />
+        </div>
+        <div className="flex lg:flex-col items-center justify-evenly w-full lg:w-1/3 lg:gap-3">
+          <StyledSkeleton styles="h-12 w-22 lg:w-60" />
+          <StyledSkeleton styles="h-12 w-22 lg:w-60" />
+          <StyledSkeleton styles="h-12 w-22 lg:w-60" />
+        </div>
       </div>
     </div>
   )
