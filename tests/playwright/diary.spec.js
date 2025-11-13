f92af5ac3623be61cba9f840908c0f9d2440db4e
Felix Bruguera <felixbruguera0@gmail.com>
2025-11-13 09:21:01 -0400

    add tests for adding collections to lists and logging media from a watchlist

--- tests/playwright/diary.spec.js
+++ tests/playwright/diary.spec.js
@@ -47,6 +47,35 @@ test.describe("as a logged in user", () => {
       await expect(page.getByText("2023-05-15")).not.toBeVisible()
     })
   })
+  test.describe("logging a movie from a watchlist removes it from the list", () => {
+    test.describe.configure({ mode: "serial" })
+    test("adding the movie to the list", async ({ page }) => {
+      await page.goto("/movies/566810")
+      await page.getByLabel("Add or remove from lists").click()
+      await page.getByRole("button", { name: "Add to list" }).click()
+      await expect(page.getByText("Succesfully Added")).toBeVisible()
+      await page.goto("/lists/c6037ab1-4c29-4cf2-939e-1a8151743f6b")
+      await expect(page.getByAltText("The Dark Kingdom")).toBeVisible()
+    })
+    test("logging the movie", async ({ page }) => {
+      await page.goto("/movies/566810")
+      await page.getByLabel("Add to your diary").click()
+      await page.getByRole("button", { name: "Save" }).click()
+      await expect(page.getByText("Succesfully Added")).toBeVisible()
+    })
+    test("checking the list after logging", async ({ page }) => {
+      await page.goto("/lists/c6037ab1-4c29-4cf2-939e-1a8151743f6b")
+      await expect(page.getByAltText("The Dark Kingdom")).not.toBeVisible()
+    })
+
+    test("deleting the log", async ({ page }) => {
+      await page.goto("/movies/566810")
+      await page.getByLabel("Manage your logs").click()
+      await page.getByRole("button", { name: "Delete" }).click()
+      await page.getByRole("button", { name: "Delete" }).click()
+      await expect(page.getByText("Succesfully deleted")).toBeVisible()
+    })
+  })
   test.describe("tv diary logs", () => {
     test.describe.configure({ mode: "serial" })
 
@@ -79,6 +108,35 @@ test.describe("as a logged in user", () => {
       await page.getByRole("button", { name: "Delete" }).click()
       await expect(page.getByText("Succesfully deleted")).toBeVisible()
       await expect(page.getByText("2023-05-15")).not.toBeVisible()
+    })
+  })
+  test.describe("logging a tv show from a watchlist removes it from the list", () => {
+    test.describe.configure({ mode: "serial" })
+    test("adding the show to the list", async ({ page }) => {
+      await page.goto("/tv/157239")
+      await page.getByLabel("Add or remove from lists").click()
+      await page.getByRole("button", { name: "Add to list" }).click()
+      await expect(page.getByText("Succesfully Added")).toBeVisible()
+      await page.goto("/lists/c6037ab1-4c29-4cf2-939e-1a8151743f6b")
+      await expect(page.getByAltText("Alien: Earth")).toBeVisible()
+    })
+    test("logging the show", async ({ page }) => {
+      await page.goto("/tv/157239")
+      await page.getByLabel("Add to your diary").click()
+      await page.getByRole("button", { name: "Save" }).click()
+      await expect(page.getByText("Succesfully Added")).toBeVisible()
+    })
+    test("checking the list after logging", async ({ page }) => {
+      await page.goto("/lists/c6037ab1-4c29-4cf2-939e-1a8151743f6b")
+      await expect(page.getByAltText("Alien: Earth")).not.toBeVisible()
+    })
+
+    test("deleting the log", async ({ page }) => {
+      await page.goto("/tv/157239")
+      await page.getByLabel("Manage your logs").click()
+      await page.getByRole("button", { name: "Delete" }).click()
+      await page.getByRole("button", { name: "Delete" }).click()
+      await expect(page.getByText("Succesfully deleted")).toBeVisible()
     })
   })
 })
